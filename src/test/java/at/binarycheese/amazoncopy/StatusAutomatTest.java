6a46fbfb5ef552ed9dd097ac5a64335e65e85a05
haha <thomas1821994@gmail.com>
2014-06-13 10:46:51 +0200

    Started testing

--- src/test/java/at/binarycheese/amazoncopy/StatusAutomatTest.java
+++ src/test/java/at/binarycheese/amazoncopy/StatusAutomatTest.java
@@ -1,7 +1,14 @@
 package at.binarycheese.amazoncopy;
+import org.junit.*;
 
 /**
  * Created by Joyce on 13.06.2014.
  */
 public class StatusAutomatTest {
+
+    @Test
+    public  void ConstructorTest(){
+        StatusAutomat automat = new StatusAutomat();
+        Assert.assertEquals(Status.UNIDENTIFIED, automat.getStatus());
+    }
 }

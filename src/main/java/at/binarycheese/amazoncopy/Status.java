abc070f345d5451a9904955de15900eae4fee795
Lukas Schneider <wiiinator1@gmail.com>
2014-06-13 10:24:00 +0200

    statuse gemacht

--- src/main/java/at/binarycheese/amazoncopy/Status.java
+++ src/main/java/at/binarycheese/amazoncopy/Status.java
@@ -2,6 +2,8 @@ package at.binarycheese.amazoncopy;
 
 /**
  * Created by Joyce on 13.06.2014.
+ * Edited by Lukas on 13.06
  */
 public enum Status {
+    UNIDENTIFIED, IDENTIFIED, PRODUCT_SELECTED, PAYMENT, PAY_ERROR, ABORT, PAID
 }

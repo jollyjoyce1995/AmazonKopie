70fe2eab0d7ad3f01b58391bcd5c2542e19509d6
haha <thomas1821994@gmail.com>
2014-06-13 10:37:31 +0200

    Added Exception, made stupid things in StatusAutomat

--- src/main/java/at/binarycheese/amazoncopy/StatusAutomat.java
+++ src/main/java/at/binarycheese/amazoncopy/StatusAutomat.java
@@ -1,7 +1,25 @@
 package at.binarycheese.amazoncopy;
 
+import at.binarycheese.amazoncopy.exceptions.StatusException;
+
 /**
  * Created by Joyce on 13.06.2014.
  */
 public class StatusAutomat {
+    private Status status;
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public StatusAutomat(){
+        this.status = Status.UNIDENTIFIED;
+    }
+
+    public void identify(){
+        if(status != Status.UNIDENTIFIED){
+            throw new StatusException("You are already identified");
+        }
+        status = Status.IDENTIFIED;
+    }
 }
---
+++ src/main/java/at/binarycheese/amazoncopy/exceptions/StatusException.java
@@ -0,0 +1,10 @@
+package at.binarycheese.amazoncopy.exceptions;
+
+/**
+ * Created by Thomas on 13.06.2014.
+ */
+public class StatusException extends RuntimeException {
+    public StatusException(String message) {
+        super(message);
+    }
+}
